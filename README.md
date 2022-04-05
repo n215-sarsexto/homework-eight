96924c22f8c901ca1b4948f78c1b7a8b26acd0c0
Summer S <snowykittin@gmail.com>
2022-04-05 01:08:15 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Homework Eight
+
+Web4 Link to Homework
+https://in-info-web4.informatics.iupui.edu/~sarsexto/homework-eight/index.html
+
+This particular assignment focuses on dynamic content. It introduces jQuery as the method for allowing for a single page to load in multiple other "pages" dynamically, without actually ever leaving to a different page. I chose to focus on a particular topic I've come to enjoy, being F1. I tried my best to use mixins, variables, and other learned content throughout the semester within this project. I found it rather easy to end up developing for this dynamic page, and really, the only question I had to ask my self was whether or not I wanted to make several pages and combine them into one or not. Ultimately, I chose not to, and we have the page I have created today!
---
+++ app/app.js
@@ -0,0 +1,168 @@
+var homeContent = ` <div class="home">
+<div id="home-banner"></div>
+<div class="home-content">
+  <h1>Formula 1: 2022 Season</h1>
+  <div class="home-text">
+    <h2>What is Formula 1? (F1)</h2>
+    <p>
+      Formula One (also known as Formula 1 or F1) is the highest class
+      of international racing for open-wheel single-seater formula
+      racing cars sanctioned by the Fédération Internationale de
+      l'Automobile (FIA). The World Drivers' Championship, which
+      became the FIA Formula One World Championship in 1981, has been
+      one of the premier forms of racing around the world since its
+      inaugural season in 1950. The word formula in the name refers to
+      the set of rules to which all participants' cars must
+      conform.[1] A Formula One season consists of a series of races,
+      known as Grands Prix, which take place worldwide on both
+      purpose-built circuits and closed public roads. (Wikipedia)
+    </p>
+  </div>
+  <div class="home-text">
+    <h2>Why am I into F1?</h2>
+    <p>
+      There are many genres of games out there in the world. Strangely
+      enough, instead of getting into most feminine games like Sims or
+      Stardew Valley, I found myself into the trenches of racing
+      games. With games such as Redout, Inertial Drift, Tackmania, and
+      even a racing minigame inside of the MMO Guild Wars 2, I've
+      found myself in love with the "zooms." It was only natural that
+      I then found myself getting into racing. Given that I live in
+      Indianapolis, it puts me very close to the Speedway race track,
+      known for the Indy 500 race. F1 is a much more complex sport
+      than the Indy 500, but being so close was an introduction to
+      what has quickly grown into an addiction to speed.
+    </p>
+  </div>
+</div>
+</div> `;
+var mclarenContent = `<div class="mclaren">
+<div id="mcbanner"></div>
+<h1>McLaren</h1>
+<h2>Racers:</h2>
+<h3>Daniel Ricciardo & Lando Norris</h3>
+
+<div class="racer">
+  <div id="norris"></div>
+  <div class="racer-description">
+    <h5>Name : Lando Norris</h5>
+    <h5>F1 Debut : 2019</h5>
+    <h5>Home Town : Glastonbury, UK</h5>
+    <p>
+      Lando Norris is one of the younger drivers in F1 currently,
+      being born in 1999. Despite being so young, he's quite a
+      phenomenal driver, having 5 podium finishes already within his
+      career. He has been known for his determination so far in the F1
+      circuit, and has developed quite a commraderie with several of
+      the other drivers. A true testament to the sport, the youngster
+      is certainly one to follow. As my personal favorite driver for this season, we'll certainly have to see where the future takes them.
+    </p>
+    <a href="https://www.mclaren.com/racing/team/lando-norris/"
+      >Find his official team page here.</a
+    >
+  </div>
+</div>
+<div class="racer">
+  <div id="ricciardo"></div>
+  <div class="racer-description">
+    <h5>Name : Daniel Ricciardo</h5>
+    <h5>F1 Debut : 2011</h5>
+    <h5>Home Town : Perth, AUS</h5>
+    <p>
+      Daniel Riccardio is an older face to the F1 sport when compared to his team mate. He has found himself shared between many of the F1 teams during his career as well. His contageous attitude and bright smile make him a very appealing racer to watch. With 8 wins and 24 other podium finishes, he's seen his share of the good, the bad, and the ugly. As a favorite of a close friend of mine, it's rather fun to root for two drivers on the same team.
+    </p>
+    <a href="https://www.mclaren.com/racing/team/daniel-ricciardo/"
+      >Find his official team page here.</a
+    >
+  </div>
+</div>`;
+var mercedesContent = `<div class="mercedes">
+          <div id="mcbanner"></div>
+          <h1>Mercedes</h1>
+          <h2>Racers:</h2>
+          <h3>Lewis Hamilton & George Russell</h3>
+
+          <div class="racer">
+            <div id="hamilton"></div>
+            <div class="racer-description">
+              <h5>Name : Lewis Hamilton</h5>
+              <h5>F1 Debut : 2007</h5>
+              <h5>Home Town : Stevenage, ENG</h5>
+              <p>
+                Lewis Hamilton is hands down one of the more accomplished drivers within F1. Known for his current 7 world-title wins within F1, he has had quite the career. With 103 total race wins, 182 podium finishes, and many other accalades to his name, Hamilton continues to be a fan-favorite around the world. I certainly agree, as Lewis Hamilton was one of the first racers that I became a fan of. His personality in press interviews is rather engaging, and he is always a fun driver to watch.
+              </p>
+              <a href="https://www.mercedesamgf1.com/en/team/drivers/lewis-hamilton/"
+                >Find his official team page here.</a
+              >
+            </div>
+          </div>
+          <div class="racer">
+            <div id="russell"></div>
+            <div class="racer-description">
+              <h5>Name : George Russell</h5>
+              <h5>F1 Debut : 2019</h5>
+              <h5>Home Town : Kings Lynn, UK</h5>
+              <p>
+                George Russell is also a newer face to F1, starting his career in the same year of Lando Norris, 2019. He has yet to have any wins under his belt, only placing on the podium once so far during his career. He is new to the Mercedes F1 team for the 2022 season, having previously raced under the Williams team. It is yet to be known how Russell will be doing in this year's grand prix, but given that he has Lewis Hamilton as a teammate, it is bound to be an interesting season for where he will go.
+              </p>
+              <a href="https://www.mercedesamgf1.com/en/team/drivers/george-russell/"
+                >Find his official team page here.</a
+              >
+            </div>
+        </div> `;
+var astonContent = `<div class="aston">
+<div id="abanner"></div>
+<h1>Aston Martin</h1>
+<h2>Racers:</h2>
+<h3>Lance Stroll & Nico Hulkenburg</h3>
+
+<div class="racer">
+  <div id="stroll"></div>
+  <div class="racer-description">
+    <h5>Name : Lance Stroll</h5>
+    <h5>F1 Debut : 2017</h5>
+    <h5>Home Town : Montreal, CAN</h5>
+    <p>
+      Lance Stroll is another youngster to F1. Beginning his career in 2017, he still has managed 3 podiums. There is quite a bit to look forward to from young Stroll. Admittedly, I'm following the Aston Martin team much more for the appearance of their cars than their drivers, so we will see how the two end up faring. So far this season, they've had a bit of a rough time in the 2022 races. Could there be a surprise upset from Stroll and his teammate?
+   </p>
+    <a href="https://www.astonmartinf1.com/en-GB/driver/lance-stroll"
+      >Find his official team page here.</a
+    >
+  </div>
+</div>
+<div class="racer">
+  <div id="hulkenburg"></div>
+  <div class="racer-description">
+    <h5>Name : Nico Hulkenburg</h5>
+    <h5>F1 Debut : 2010</h5>
+    <h5>Home Town : Emmerich am Rhein, GER</h5>
+    <p>
+      Nico Hulkenburg is the reserve F1 driver for the Aston Martin team. As such, he has no official page on their website, and is substituting in this season for Sebastian Vettel. It will be an interesting season, as the racer has previously raced under several teams such as Williams, Renault, and Racing Point. He has not had any known podium finishes, but has had an overall stable career.
+    </p>
+    <a href="https://en.wikipedia.org/wiki/Nico_H%C3%BClkenberg"
+      >Find his wikipedia page here.</a
+    >
+  </div>
+</div> `;
+
+function initListeners(){
+    $("nav a").click(function(e){
+        let btnID = e.currentTarget.id;
+        loadPageContent(btnID);
+        //console.log("Click!" + btnID);
+    });
+}
+
+function loadPageContent(pageName){
+    let contentID = pageName + "Content";
+    $("#app").html(eval(contentID));
+}
+
+function initApp(){
+    initListeners();
+    loadPageContent("home");
+}
+
+$(document).ready(function(){
+    initApp();
+});
